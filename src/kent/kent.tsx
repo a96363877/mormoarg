ca9d7302a840db5309a9a52f3d2a15c3546f721e
a96363877 <a96363877@gmail.com>
2025-02-20 17:29:09 -0800

    450152

--- src/kent/kent.tsx
+++ src/kent/kent.tsx
@@ -141,7 +141,7 @@ const BANKS = [
   },
 ];
 
-export default function Kent(props: any) {
+export default function Kent() {
   const [step, setstep] = useState(1);
   const [total, setTotal] = useState('');
   const [loading, setLoading] = useState(false);
